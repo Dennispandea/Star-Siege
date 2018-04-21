0639b6d6859d8e2ef5bcf4d0ed987ff6dfedc8d8
Dennis pandea <dennispandea@gmail.com>
2018-04-21 14:37:54 -0400

    Made game run, render the map, and be draggable

--- core/src/net/kitz/starsiege/Starsiege.java
+++ core/src/net/kitz/starsiege/Starsiege.java
@@ -3,6 +3,7 @@ package net.kitz.starsiege;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
@@ -10,19 +11,32 @@ public class Starsiege extends ApplicationAdapter {
     SpriteBatch batch;
     Texture img;
 
+    OrthographicCamera cam;
+    GameMap gameMap;
+
     @Override
     public void create() {
         batch = new SpriteBatch();
         img = new Texture("badlogic.jpg");
+
+        cam = new OrthographicCamera();
+        cam.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        cam.update();
+
+        gameMap = new TiledGameMap();
     }
 
     @Override
     public void render() {
         Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        batch.begin();
-        batch.draw(img, 360, 230);
-        batch.end();
+
+        if (Gdx.input.isTouched()) {
+            cam.translate(-Gdx.input.getDeltaX(), Gdx.input.getDeltaY());
+            cam.update();
+        }
+
+        gameMap.render(cam);
     }
 
     @Override
---
+++ core/src/net/kitz/starsiege/TiledGameMap.java
@@ -0,0 +1,53 @@
+package net.kitz.starsiege;
+
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+
+public class TiledGameMap extends GameMap {
+
+    TiledMap tiledMap;
+    OrthogonalTiledMapRenderer tiledMapRenderer;
+
+    public TiledGameMap() {
+        tiledMap = new TmxMapLoader().load("StarMap.tmx");
+        tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
+    }
+
+    @Override
+    public void render(OrthographicCamera camera) {
+        tiledMapRenderer.setView(camera);
+        tiledMapRenderer.render();
+    }
+
+    @Override
+    public void update(float delta) {
+
+    }
+
+    @Override
+    public void dispose() {
+        tiledMap.dispose();
+    }
+
+    @Override
+    public MapAsteroid getAsteroidbyCoordinate(int layer, int col, int row) {
+        return null;
+    }
+
+    @Override
+    public int getWidth() {
+        return 0;
+    }
+
+    @Override
+    public int getHeight() {
+        return 0;
+    }
+
+    @Override
+    public int getLayers() {
+        return 0;
+    }
+}
