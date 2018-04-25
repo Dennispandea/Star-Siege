f92a6f8b3ef6815c49a18f179939713e11d9c692
Dennis <dennispandea@gmail.com>
2018-04-25 10:47:20 -0400

    Moving over from raw textures to using sprites

--- core/src/net/kitz/starsiege/entities/Player.java
+++ core/src/net/kitz/starsiege/entities/Player.java
@@ -3,8 +3,8 @@ package net.kitz.starsiege.entities;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Vector2;
 import net.kitz.starsiege.world.GameMap;
 
 public class Player extends Entity {
@@ -12,11 +12,13 @@ public class Player extends Entity {
     private int nPlayerSpeed = 4;
     private int nPlayerRotSpeed;
 
-    Texture image;
+    Texture txImage;
+    Sprite SprPlayer;
 
     public Player(float x, float y, GameMap map) {
         super(x, y, EntityType.PLAYER, map);
-        image = new Texture("Ship.png");
+        SprPlayer = new Sprite(txImage, 16, 22);
+        txImage = new Texture("Ship.png");
     }
 
     @Override
@@ -38,12 +40,12 @@ public class Player extends Entity {
             this.dVelocityX += -nPlayerSpeed / getdMass();
         }
         if (Gdx.input.isKeyPressed(Input.Keys.SPACE)) {
-            if (this.dVelocityX > 1 || this.dVelocityY > 1) {
-                this.dVelocityY *= 0.95;
-                this.dVelocityX *= 0.95;
-            } else {
+            if (this.dVelocityX < 1 && this.dVelocityY < 1) {
                 this.dVelocityY = 0;
                 this.dVelocityX = 0;
+            } else {
+                this.dVelocityY *= 0.95;
+                this.dVelocityX *= 0.95;
             }
         }
         super.update(fDeltaTime, fGravity);
@@ -51,6 +53,9 @@ public class Player extends Entity {
 
     @Override
     public void render(SpriteBatch batch) {
-        batch.draw(image, pos.x, pos.y, getnWidth(), getnLength());
+        batch.draw(SprPlayer, getPos().x, getPos().y, getnWidth(), getnLength());
+
+
     }
+
 }
